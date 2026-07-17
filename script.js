8fa76f528008ac1bd5ae5c17d15f939082f19751
Thameemkm1 <162680763+Thameemkm1@users.noreply.github.com>
2026-07-17 21:30:55 +0530

    Update script.js

--- script.js
+++ script.js
@@ -3,4 +3,10 @@ const toggle = document.querySelector(".menu-toggle");
 
 toggle.addEventListener("click", () => {
     menu.classList.toggle("active");
+
+    if(menu.classList.contains("active")){
+        toggle.innerHTML = "✕";
+    }else{
+        toggle.innerHTML = "☰";
+    }
 });
