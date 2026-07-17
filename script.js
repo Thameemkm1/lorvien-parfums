21f1fa64fdaaeb0171fb0ba5c0350fc76dd01d1a
Thameemkm1 <162680763+Thameemkm1@users.noreply.github.com>
2026-07-17 22:15:19 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,20 +1,31 @@
-const menuToggle = document.querySelector(".menu-toggle");
-const nav = document.querySelector("nav");
+// ==========================
+// LORVIEN PARFUMS
+// script.js
+// ==========================
 
-menuToggle.addEventListener("click", () => {
-    nav.classList.toggle("active");
+const menuBtn = document.getElementById("menuBtn");
+const menu = document.getElementById("menu");
 
-    if (nav.classList.contains("active")) {
-        menuToggle.textContent = "✕";
-    } else {
-        menuToggle.textContent = "☰";
-    }
+menuBtn.addEventListener("click", () => {
+    menu.classList.toggle("active");
 });
 
-// Menu link അമർത്തുമ്പോൾ menu അടയ്ക്കുക
-document.querySelectorAll("nav a").forEach(link => {
+document.querySelectorAll(".menu a").forEach(link => {
     link.addEventListener("click", () => {
-        nav.classList.remove("active");
-        menuToggle.textContent = "☰";
+        menu.classList.remove("active");
     });
+});
+
+const header = document.querySelector(".header");
+
+window.addEventListener("scroll", () => {
+
+    if (window.scrollY > 50) {
+        header.style.background = "rgba(0,0,0,.85)";
+        header.style.backdropFilter = "blur(16px)";
+    } else {
+        header.style.background = "rgba(0,0,0,.25)";
+        header.style.backdropFilter = "blur(12px)";
+    }
+
 });
