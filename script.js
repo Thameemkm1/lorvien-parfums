87665fb4b2446c80369980cdb99dd5c2d1adb9c9
Thameemkm1 <162680763+Thameemkm1@users.noreply.github.com>
2026-07-17 21:28:45 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,14 +1,6 @@
 const menu = document.querySelector("nav");
 const toggle = document.querySelector(".menu-toggle");
 
-toggle.addEventListener("click", function () {
-
-  if (window.getComputedStyle(menu).display === "none") {
-    menu.style.display = "flex";
-    menu.style.flexDirection = "column";
-    menu.style.alignItems = "center";
-  } else {
-    menu.style.display = "none";
-  }
-
+toggle.addEventListener("click", () => {
+    menu.classList.toggle("active");
 });
