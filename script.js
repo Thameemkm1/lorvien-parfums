39d65cf6202e4ed76e2f38070f0bc31818b443d2
Thameemkm1 <162680763+Thameemkm1@users.noreply.github.com>
2026-07-18 22:33:03 +0530

    Update script.js

--- script.js
+++ script.js
@@ -15,19 +15,3 @@ document.querySelectorAll(".menu a").forEach(link => {
         menu.classList.remove("active");
     });
 });
-
-const header = document.querySelector(".header");
-
-window.addEventListener("scroll", () => {
-
-    if (window.scrollY > 50) {
-        header.style.background = "rgba(0,0,0,.85)";
-        header.style.backdropFilter = "blur(16px)";
-    } else {
-        header.style.background = "rgba(0,0,0,.25)";
-        header.style.backdropFilter = "blur(12px)";
-    }
-
-});
-
-});
